886715fbd670ea6348274bdd34bbbfce11058d73
dr4g0n96 <simon@stateless.digital>
2022-08-31 23:05:18 +0200

    Continue with Intro section

--- pages/intro.tsx
+++ pages/intro.tsx
@@ -1,37 +1,81 @@
 import type { NextPage } from "next";
 import Image from "next/image";
 import styles from "../styles/Intro.module.css";
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion";
 
-const imageContainer = {
+const imageContainer: Variants = {
   show: (nr: number) => ({
-    y: "0%",
+    y: `${nr === 1 ? 0 : 15}%`,
     zIndex: 300 / nr,
-    height: `${100 + (nr - 1) * 10}%`,
+    // height: `${110 * nr}%`,
+    height: `${100 + (nr - 1) * 15}%`,
   }),
   slide: (nr: number) => ({
-    y: "-100%",
+    y: [nr === 1 ? "0%" : "15%", "0%", "-100%"],
     transition: {
-      duration: 1,
-      delay: 1 * nr + 0.1 * (nr - 1),
+      duration: nr === 1 ? 1.5 : 3, // 1: 1, 2: 2.1 3: 3.2
+      times: [0, nr === 1 ? 0 : 0.5, 1], // 1: 0 2: 1.1 3: 1.1
+      // delay: 1 * nr + 0.1 * (nr - 1),
+      delay: nr === 3 ? 3 : 1.5,
       ease: [0.77, 0, 0.175, 1],
     },
   }),
 };
 
+//     1s -> 2s -> 3s -> 4s
+// p1: -- -> ^^ -> -- -> --
+// p2: -- -> ^  -> ^^ -> --
+// p3: -- -> -- -> ^  -> ^^
+
 const Intro: NextPage = () => {
   return (
     <div className={styles.container}>
+      <div className={styles.contentContainer}>
+        <div className={styles.maxWContainer}>
+          <div className={styles.personContainer}>
+            <div className={styles.person}>
+              {/* <Image
+              src="/headshot.jpeg"
+              alt=""
+              width="245"
+              height="320"
+              layout="responsive"
+            /> */}
+              <img src="/headshot.jpeg" alt="" />
+            </div>
+            <div className={styles.person} style={{ marginTop: "96px" }}>
+              {/* <Image
+              src="/headshot.jpeg"
+              alt=""
+              width="245"
+              height="320"
+              layout="responsive"
+            /> */}
+              <img src="/headshot.jpeg" alt="" />
+            </div>
+            <div className={styles.person}>
+              {/* <Image
+              src="/headshot.jpeg"
+              alt=""
+              width="245"
+              height="320"
+              layout="responsive"
+            /> */}
+              <img src="/headshot.jpeg" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
       <motion.div
         className={styles.images}
-        initial={{ y: "0%" }}
-        animate={{
-          y: "-20%",
-          transition: {
-            duration: 2.2,
-            delay: 1,
-          },
-        }}
+        // initial={{ y: "0%" }}
+        // animate={{
+        //   y: "-20%",
+        //   transition: {
+        //     duration: 2.2,
+        //     delay: 1,
+        //   },
+        // }}
       >
         <motion.div
           custom={1}
